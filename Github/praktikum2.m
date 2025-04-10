379ce7c5c2eb773fa8bf50ec089abf0234493e26
EkaristiMontoh <Kristimontoh@gmail.com>
2025-04-10 10:02:08 -0800

    coba pertama

---
+++ Github/praktikum2.m
@@ -0,0 +1,17 @@
+clc
+clear
+%-> memberi keterangan
+%menghitungluaspersegipanjang
+p = 2
+l = 20
+lp = p*l
+disp(lp);
+
+%penjumlahan
+lp1 = p+l
+lp2 = p-l
+lp3 = p/l
+
+disp(lp1);
+disp(lp2);
+disp(lp3);
